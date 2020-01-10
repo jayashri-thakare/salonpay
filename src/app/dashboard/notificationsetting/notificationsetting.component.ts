3e8f7f8b42cb351a195a5c050a1c95f04c4d7d4e
jayashri-thakare <jayashri.thakare@gmail.com>
2020-01-10 18:59:18 +0530

    added user account function

--- src/app/dashboard/notificationsetting/notificationsetting.component.ts
+++ src/app/dashboard/notificationsetting/notificationsetting.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {UserdataService} from '../../userdata.service';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {Observable} from 'rxjs';
+
+// tslint:disable-next-line:class-name
+class userdetail {
+}
 
 @Component({
   selector: 'app-notificationsetting',
@@ -15,6 +20,7 @@ export class NotificationsettingComponent implements OnInit {
   private notificationobject = {};
   private EmailNotification = {};
   private SMSNotification = {};
+  private notifications: Observable<userdetail>;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private userdataService: UserdataService) { }
 
@@ -31,10 +37,17 @@ export class NotificationsettingComponent implements OnInit {
       EnableTextNotification: [true],
       Notification: this.notificationobject
     });
+    this.getNotifications();
   }
 
+  getNotifications() {
+    this.userdataService.getNotification().subscribe((data) => {
+      console.log(data)
+      this.notifications = data;
+    });
+  }
   notificationsetting(notify, type){
-    debugger;
+    // debugger;
     if(type === 'email'){
       this.EmailNotification['Email'] = notify.currentTarget.checked;
     }else if(type === 'sms'){
@@ -49,6 +62,7 @@ export class NotificationsettingComponent implements OnInit {
   }
 
   updateNotification(userdata) {
+    debugger;
     console.log(userdata, this.notificationForm)
     // tslint:disable-next-line:triple-equals
     if (this.notificationForm.status == 'VALID') {
